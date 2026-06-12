97bfdc9595d7a2edf7ed1cb7ccd074d086ccee49
Bradley Fowler <bradleyfowler1234@gmail.com>
2026-06-12 16:34:22 +0100

    feat: add styles to the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header>
-      <Image
-        src="/logo-small.svg"
-        alt="Savings Tracker Logo"
-        width={32}
-        height={32}
-      />
-      <h1>Savings Tracker</h1>
-      <button>+ New Goal</button>
+    <header className="flex items-center justify-between h-18 border-b-gray-500 border-b">
+      <div className="flex items-center gap-3 ml-4">
+        <Image
+          src="/logo-small.svg"
+          alt="Savings Tracker Logo"
+          width={32}
+          height={32}
+        />
+        <h1 className="text-xl tracking-[0.3px] font-bold">Savings Tracker</h1>
+      </div>
+      <button className="mr-4">+ New Goal</button>
     </header>
   );
 };
