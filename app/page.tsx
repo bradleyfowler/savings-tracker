2941f3b4c0ad61365b59386c7011e09a3d7154c5
Bradley Fowler <bradleyfowler1234@gmail.com>
2026-06-19 15:57:32 +0100

    feat: add styles to goal card

--- app/page.tsx
+++ app/page.tsx
@@ -16,56 +16,61 @@ export default function Home() {
           <DepositCard />
         </section>
         <section>
-          <h1 className="text-[32px] font-bold mb-5">Your goals</h1>
-          <ListControls />
-          <GoalCard
-            title="Macbook Pro M4"
-            progress={76}
-            amount="£1,900 of £2,499"
-            dueDate="1 Jun 2026"
-          />
-          <GoalCard
-            title="Mechanical Keyboard"
-            progress={100}
-            amount="£350 of £350"
-            dueDate="15 March 2026"
-          />
-          <GoalCard
-            title="React Conf Trip"
-            progress={72}
-            amount="£1,299 of £1,800"
-            dueDate="15 May 2026"
-          />
-          <GoalCard
-            title="Anniversary Trip to Italy"
-            progress={60}
-            amount="£2,400 of £4,000"
-            dueDate="15 Sep 2026"
-          />
-          <GoalCard
-            title="4K Monitor Upgrade"
-            progress={75}
-            amount="£450 of £600"
-            dueDate="30 Apr 2026"
-          />
-          <GoalCard
-            title="Emergency Fund"
-            progress={80}
-            amount="£4,000 of £5,000"
-            dueDate="No deadline"
-          />
-          <GoalCard
-            title="New Road Bike"
-            progress={57}
-            amount="£850 of £1,500"
-            dueDate="1 May 2026"
-          />
-          <GoalCard
-            title="Ergonomic Chair"
-            progress={0}
-            amount="£0 of £1,200"
-            dueDate="1 Aug 2026"
-          />
+          <div className="mb-5">
+            <h1 className="text-[32px] font-bold">Your goals</h1>
+            <ListControls />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <GoalCard
+              title="Macbook Pro M4"
+              progress={76}
+              amount="£1,900 of £2,499"
+              dueDate="1 Jun 2026"
+            />
+            <GoalCard
+              complete={true}
+              title="Mechanical Keyboard"
+              progress={100}
+              amount="£350 of £350"
+              dueDate="15 March 2026"
+            />
+            <GoalCard
+              title="React Conf Trip"
+              progress={72}
+              amount="£1,299 of £1,800"
+              dueDate="15 May 2026"
+            />
+            <GoalCard
+              title="Anniversary Trip to Italy"
+              progress={60}
+              amount="£2,400 of £4,000"
+              dueDate="15 Sep 2026"
+            />
+            <GoalCard
+              title="4K Monitor Upgrade"
+              progress={75}
+              amount="£450 of £600"
+              dueDate="30 Apr 2026"
+            />
+            <GoalCard
+              title="Emergency Fund"
+              progress={80}
+              amount="£4,000 of £5,000"
+              dueDate="No deadline"
+            />
+            <GoalCard
+              title="New Road Bike"
+              progress={57}
+              amount="£850 of £1,500"
+              dueDate="1 May 2026"
+            />
+            <GoalCard
+              title="Ergonomic Chair"
+              progress={0}
+              amount="£0 of £1,200"
+              dueDate="1 Aug 2026"
+            />
+          </div>
         </section>
       </main>
     </div>
--- components/GoalCard/GoalCard.tsx
+++ components/GoalCard/GoalCard.tsx
@@ -3,19 +3,32 @@ const GoalCard = ({
   dueDate,
   progress,
   title,
+  complete,
 }: {
   amount: string;
   dueDate: string;
   progress: number;
   title: string;
+  complete?: boolean;
 }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{progress}%</p>
-      <progress value={progress} max="100"></progress>
-      <p>{amount}</p>
-      <p>Due {dueDate}</p>
+    <div className="rounded-2xl bg-[#1f1f1f] bg-[url('/pattern-grid.svg')] border border-solid border-[#3C3B40]">
+      <div className="flex justify-between align-middle p-4 mb-8">
+        <h1 className="text-xl font-bold">{title}</h1>
+        {complete && (
+          <span className="text-[#4ADE80] bg-[#1A3D2B] border-2 border-solid border-[#4ADE80] rounded-full font-bold py-1.5 px-2.5 text-sm">
+            COMPLETE
+          </span>
+        )}
+      </div>
+      <div className="p-4">
+        <p className="text-[44px] mb-4 font-bold">{progress}%</p>
+        <progress value={progress} max="100"></progress>
+        <div className="flex gap-4">
+          <p>{amount}</p>
+          <p>Due {dueDate}</p>
+        </div>
+      </div>
     </div>
   );
 };
